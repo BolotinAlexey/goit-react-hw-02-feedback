14350ae7da1d09a298cb062f338f7033b6bc8207
Oleksii Bolotin <samkiral73@gmail.com>
2023-03-07 17:50:46 +0100

    add StatisticElement

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,71 @@
-// import data from "data";
-// import transactions from "transactions";
-// import friends from "friends";
-import Feadback from 'components/Feadback/Feadback';
-// import Statistics from "components/Statistics/Statistics";
-// import FriendList from "components/FriendList/FriendList";
-// import TransactionHistory from "components/TransactionHistory/TransactionHistory";
+// import user from 'user';
+// import data from 'data';
+// import transactions from 'transactions';
+// import friends from 'friends';
+// import Profile from 'components/Profile/Profile';
+// import Statistics from 'components/Statistics/Statistics';
+// import FriendList from 'components/FriendList/FriendList';
+import StatisticElement from 'components/StatisticElement/StatisticElement';
+import React, { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        // height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '30px',
-        // flexWrap: 'wrap',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <Feadback />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  goodInc = () =>
+    this.setState(({ good }) => ({
+      good: ++good,
+    }));
+  neutralInc = () =>
+    this.setState(({ neutral }) => ({
+      neutral: ++neutral,
+    }));
+  badInc = () =>
+    this.setState(({ bad }) => ({
+      bad: ++bad,
+    }));
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return bad + good + neutral;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    return this.countTotalFeedback()
+      ? Math.round((good * 100) / this.countTotalFeedback())
+      : 0;
+  };
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+        <section className="btns">
+          <h2>Please leave feadback</h2>
+          <ul className="buttons">
+            <li>
+              <button onClick={this.goodInc}>Good</button>
+            </li>
+            <li>
+              <button onClick={this.neutralInc}>Neutral</button>
+            </li>
+            <li>
+              <button onClick={this.badInc}>Bad</button>
+            </li>
+          </ul>
+        </section>
+        <StatisticElement
+          {...this.state}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()}
+        />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/StatisticElement/StatisticElement.jsx
@@ -0,0 +1,29 @@
+// import PropTypes from 'prop-types';
+// import css from './StatisticElement.module.css';
+
+function StatisticElement({ good, neutral, bad, total, positivePercentage }) {
+  return (
+    <section className="statistics">
+      <h2>Statisticks</h2>
+      <ul>
+        <li>
+          <p>Good: {good}</p>
+        </li>
+        <li>
+          <p>Neutral: {neutral}</p>
+        </li>
+        <li>
+          <p>Bad: {bad}</p>
+        </li>
+        <li>
+          <p>Total: {total}</p>
+        </li>
+        <li>
+          <p>Positive feedback: {positivePercentage}%</p>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default StatisticElement;
